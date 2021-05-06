cb520ffbbf5c5b49e2853db1d3be527961d1f5cb
Murad718 <66924369+Murad718@users.noreply.github.com>
2021-05-06 16:03:08 +0300

    Wear1

---
+++ WearHouse/js/test6.js
@@ -0,0 +1,39 @@
+ "use strict"
+
+function DropM(){
+	Burger();
+	Cross();
+}
+
+function Cross(){
+	let cross = document.getElementById("NavCross");
+	cross.onclick = UnDrop;
+}
+
+function UnDrop(){
+	let menu = document.getElementById("dropdownMenu");
+	let cross = document.getElementById("NavCross");
+	let burger = document.getElementById("NavBurger");
+
+	burger.classList.remove("active");
+	cross.classList.add("active");
+	menu.classList.remove("active");
+}
+
+function Drop(){
+	let menu = document.getElementById("dropdownMenu");
+	let cross = document.getElementById("NavCross");
+	let burger = document.getElementById("NavBurger");
+
+	burger.classList.add("active");
+	cross.classList.remove("active");
+	menu.classList.add("active");
+}
+
+function Burger(){
+	let burger = document.getElementById("NavBurger");
+
+	burger.onclick = Drop;
+}
+
+DropM();
